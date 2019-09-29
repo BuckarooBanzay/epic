3cedeac65906e90c30993a6da14ad650e38cf9d3
Thomas Rudin <thomas@rudin.io>
2019-09-29 19:52:55 +0200

    waypoint cfg

--- blocks/waypoint.lua
+++ blocks/waypoint.lua
@@ -1,4 +1,26 @@
 
+-- playername => pos
+local punch_handler = {}
+
+local update_formspec = function(meta, pos)
+	local pos = meta:get_string("pos")
+	local radius = meta:get_int("radius")
+
+	meta:set_string("infotext", "Waypoint block: pos=" .. pos ..
+		", radius=" .. radius)
+
+	meta:set_string("formspec", "size[8,3;]" ..
+		-- col 1
+		"field[0.2,0.5;8,1;radius;Radius;" .. radius .. "]" ..
+
+		-- col 2
+		"button_exit[0.1,1.5;8,1;setpos;Set position]" ..
+
+		-- col 2
+		"button_exit[0.1,2.5;8,1;save;Save]" ..
+		"")
+end
+
 minetest.register_node("epic:waypoint", {
 	description = "Epic waypoint block",
 	tiles = {
@@ -13,9 +35,43 @@ minetest.register_node("epic:waypoint", {
 	groups = {cracky=3,oddly_breakable_by_hand=3},
 	on_rotate = screwdriver.rotate_simple,
 
+	on_construct = function(pos)
+    local meta = minetest.get_meta(pos)
+		meta:set_string("pos", minetest.pos_to_string(pos))
+		meta:set_int("radius", 3)
+    update_formspec(meta, pos)
+  end,
+
+  on_receive_fields = function(pos, formname, fields, sender)
+    local meta = minetest.get_meta(pos);
+
+		if not sender or minetest.is_protected(pos, sender:get_player_name()) then
+			-- not allowed
+			return
+		end
+
+    if fields.save then
+			local radius = tonumber(fields.radius) or 3
+			if radius < 0 then
+				radius = 1
+			end
+
+			meta:set_int("radius", radius)
+			update_formspec(meta, pos)
+    end
+
+		if fields.setpos then
+			minetest.chat_send_player(sender:get_player_name(), "[epic] Please punch the desired target position")
+			punch_handler[sender:get_player_name()] = pos
+		end
+
+  end,
+
 	epic = {
     on_enter = function(pos, meta, player, ctx)
-			local target_pos = { x=0, y=0, z=0 }
+			local target_pos = minetest.string_to_pos(meta:get_string("pos"))
+			ctx.step_data.pos = target_pos
+			ctx.step_data.radius = meta:get_int("radius")
 
 			ctx.step_data.waypoint_hud_id = player:hud_add({
 				hud_elem_type = "waypoint",
@@ -27,8 +83,7 @@ minetest.register_node("epic:waypoint", {
     end,
     on_check = function(pos, meta, player, ctx)
 			local pos = player:get_pos()
-			local target_pos = { x=0, y=0, z=0 }
-			if vector.distance(pos, target_pos) < 3 then
+			if vector.distance(pos, ctx.step_data.pos) < ctx.step_data.radius then
 				ctx.next()
 			end
     end,
@@ -37,3 +92,21 @@ minetest.register_node("epic:waypoint", {
     end
   }
 })
+
+minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
+	local playername = puncher:get_player_name()
+	local cfg_pos = punch_handler[playername]
+	if cfg_pos then
+		local meta = minetest.get_meta(cfg_pos)
+		local pos_str = minetest.pos_to_string(pos)
+		meta:set_string("pos", pos_str)
+		minetest.chat_send_player(playername, "[epic] target position successfully set to " .. pos_str)
+		punch_handler[playername] = nil
+	end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	local playername = player:get_player_name()
+	punch_handler[playername] = nil
+
+end)
