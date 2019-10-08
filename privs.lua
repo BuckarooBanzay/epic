677b0223bbb481c59af09d484c57988b2b29fc18
BuckarooBanzay <BuckarooBanzay@github.com>
2019-10-08 13:02:20 +0200

    check for epic_admin

--- privs.lua
+++ privs.lua
@@ -3,3 +3,8 @@ minetest.register_privilege("epic_debug", {
   description = "can call any epic function",
   give_to_singleplayer = true
 })
+
+minetest.register_privilege("epic_admin", {
+  description = "can administer any epic function",
+  give_to_singleplayer = true
+})
