39e2582c20cef4f4d1c9c1c7e6bb183ee866d3a7
Thomas Rudin <thomas@rudin.io>
2019-10-22 21:48:07 +0200

    stats

---
+++ blocks/stats.lua
@@ -0,0 +1,53 @@
+local update_formspec = function(meta)
+	local counter = meta:get_int("counter")
+	meta:set_string("infotext", "Visits: " .. counter)
+
+	meta:set_string("formspec", "size[8,2;]" ..
+		"label[0,0.5;Visits: " .. counter .. "]" ..
+		"button_exit[0.1,1.5;8,1;reset;Reset]" ..
+		"")
+end
+
+minetest.register_node("epic:stats", {
+	description = "Epic stats block",
+	tiles = {
+		"epic_node_bg.png",
+		"epic_node_bg.png",
+		"epic_node_bg.png",
+		"epic_node_bg.png",
+		"epic_node_bg.png",
+		"epic_node_bg.png^epic_stats.png",
+	},
+	paramtype2 = "facedir",
+	groups = {cracky=3,oddly_breakable_by_hand=3,epic=1},
+	on_rotate = screwdriver.rotate_simple,
+
+	on_construct = function(pos)
+    local meta = minetest.get_meta(pos)
+		meta:set_int("counter", 0)
+    update_formspec(meta)
+  end,
+
+	on_receive_fields = function(pos, _, fields, sender)
+		if not sender or minetest.is_protected(pos, sender:get_player_name()) then
+			-- not allowed
+			return
+		end
+
+		local meta = minetest.get_meta(pos);
+
+		if fields.reset then
+			meta:set_int("counter", 0)
+			update_formspec(meta)
+		end
+
+	end,
+
+  epic = {
+    on_enter = function(_, meta, _, ctx)
+			meta:set_int("counter", meta:get_int("counter") + 1)
+			update_formspec(meta)
+			ctx.next()
+    end
+  }
+})
