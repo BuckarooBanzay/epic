7d4af88a6aea2d0821aae372893150f4d41627a0
BuckarooBanzay <BuckarooBanzay@github.com>
2019-10-28 14:50:19 +0100

    stash all option

--- blocks/stash_inventory.lua
+++ blocks/stash_inventory.lua
@@ -56,18 +56,21 @@ minetest.register_node("epic:stash_inv", {
       local filter_map = {}
 			local inv = meta:get_inventory()
       local filter_items = inv:get_list("main")
+      local filter_items_count = 0
       for _, filter_item in ipairs(filter_items) do
         local node_name = filter_item:get_name()
         if node_name and node_name ~= "" then
+          filter_items_count = filter_items_count + 1
           filter_map[node_name] = true
         end
       end
 
       local player_inv = player:get_inventory()
+      local filter_all = filter_items_count == 0 -- no filter means stash EVERYTHING!
       local i = 1
       while i <= player_inv:get_size("main") do
         local stack = player_inv:get_stack("main", i)
-        if filter_map[stack:get_name()] then
+        if filter_all or filter_map[stack:get_name()] then
           player_inv:set_stack("main", i, ItemStack(nil))
           table.insert(ctx.data.stashed_items, stack:to_string())
         end
