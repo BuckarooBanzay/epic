c2942bffbdab1a217360619f15d5fa575cd524b7
BuckarooBanzay <BuckarooBanzay@github.com>
2019-11-07 09:12:44 +0100

    embed minetest.conf

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # simple integration test
 
+cat <<EOF > /tmp/minetest.conf
+ enable_epic_integration_test = true
+EOF
+
 mkdir -p /tmp/mt/worlds/world
 chmod 777 /tmp/mt -Rv
 docker run --rm -it \
-	-v $(pwd)/test-minetest.conf:/etc/minetest/minetest.conf \
+	-v /tmp/minetest.conf:/etc/minetest/minetest.conf:ro \
 	-v /tmp/mt:/var/lib/minetest/.minetest \
 	-v $(pwd):/var/lib/minetest/.minetest/worlds/world/worldmods/epic \
 	registry.gitlab.com/minetest/minetest/server:5.0.1
