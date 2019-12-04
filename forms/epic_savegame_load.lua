603ac59745e9ebcf7cadd73a4b48abe15562c835
BuckarooBanzay <BuckarooBanzay@github.com>
2019-12-04 07:09:55 +0100

    load game form with single click and "load" button

--- forms/epic_savegame_load.lua
+++ forms/epic_savegame_load.lua
@@ -1,6 +1,14 @@
 
 local FORMNAME = "epic_savegame_load"
 
+-- playername -> id
+local player_selections = {}
+
+minetest.register_on_leaveplayer(function(player)
+	local playername = player:get_player_name()
+	player_selections[playername] = nil
+end)
+
 epic.form.epic_savegame_load = function(topic, playername)
 	local savegame = epic.savegame.load(playername)
 	local levels = savegame[topic] or {}
@@ -12,7 +20,8 @@ epic.form.epic_savegame_load = function(topic, playername)
 
 	local formspec = "size[8,8;]" ..
 		"label[0,0;Epic savegame load]" ..
-		"textlist[0,0.5;7.8,7.5;levelname;" .. list .. "]"
+		"textlist[0,0.5;7.8,6.5;levelname;" .. list .. "]" ..
+		"button_exit[0.1,7.2;8,1;load;Load]"
 
 	minetest.show_formspec(playername,
 		FORMNAME .. ";" .. topic,
@@ -31,45 +40,55 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 	local topic = parts[2]
 	local playername = player:get_player_name()
 
+	if fields.load then
+		local savegame = epic.savegame.load(playername)
+		local levels = savegame[topic] or {}
+
+		local selected_level = player_selections[playername]
+		if not selected_level then
+			return true
+		end
+
+		local selected_pos = nil
+
+		local i = 1
+		for _, pos in pairs(levels) do
+			if selected_level == i then
+				selected_pos = pos
+			end
+			i = i + 1
+		end
+
+		if not selected_pos then
+			return true
+		end
+
+		local node = epic.get_node(selected_pos)
+		if node.name ~= "epic:save" then
+			return true
+		end
+
+		local meta = minetest.get_meta(selected_pos)
+		local target_pos = minetest.string_to_pos(meta:get_string("pos"))
+
+		if not target_pos then
+			return true
+		end
+
+		local destination_pos = epic.to_absolute_pos(selected_pos, target_pos)
+		player:set_pos(destination_pos)
+	end
+
 	if fields.levelname then
 		local lvl_parts = fields.levelname:split(":")
-		if lvl_parts[1] == "DCL" then
+		if lvl_parts[1] == "CHG" then
 			local selected_level = tonumber(lvl_parts[2])
 
 			if not selected_level then
 				return true
 			end
 
-			local savegame = epic.savegame.load(playername)
-			local levels = savegame[topic] or {}
-			local selected_pos = nil
-
-			local i = 1
-			for _, pos in pairs(levels) do
-				if selected_level == i then
-					selected_pos = pos
-				end
-				i = i + 1
-			end
-
-			if not selected_pos then
-				return true
-			end
-
-			local node = epic.get_node(selected_pos)
-			if node.name ~= "epic:save" then
-				return true
-			end
-
-			local meta = minetest.get_meta(selected_pos)
-			local target_pos = minetest.string_to_pos(meta:get_string("pos"))
-
-			if not target_pos then
-				return true
-			end
-
-			local destination_pos = epic.to_absolute_pos(selected_pos, target_pos)
-			player:set_pos(destination_pos)
+			player_selections[playername] = selected_level
 		end
 	end
 
